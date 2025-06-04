5f76a5772c12fbdac029fe2277cf9fdb40f49a8d
Sasszem <barath.laszlo.szolnok@gmail.com>
2025-06-04 15:28:54 +0200

    First draft testing wiring

---
+++ digibox.ino
@@ -0,0 +1,105 @@
+/*
+  HA5KFU-DIGRIG-ESP
+*/
+
+const auto PIN_RX = D2;
+const auto PIN_TX = D0;
+const auto PIN_PTT = D1; 
+
+#include <SoftwareSerial.h>
+SoftwareSerial radioSer(PIN_RX, PIN_TX, false);
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(PIN_RX, INPUT);
+  pinMode(PIN_PTT, INPUT_PULLUP);
+  Serial.begin(115200);
+  radioSer.begin(9600);
+  delay(500);
+  readFreqTask();
+}
+
+void taskHeartbeat() {
+  static uint32_t last_update;
+  static bool state;
+  if (millis() - last_update > 500) {
+    state = !state;
+    digitalWrite(LED_BUILTIN, state);
+    //Serial.println(state ? "ON" : "OFF");
+    last_update = millis();
+  }
+}
+
+void taskPTTDetect() {
+  bool is_ptt = ! digitalRead(PIN_PTT);
+}
+
+int read_with_wait() {
+  while(!radioSer.available());
+  return radioSer.read();
+}
+
+void readFreqTask() {
+  while(radioSer.read() > 0);
+  radioSer.write(0);
+  radioSer.write(0);
+  radioSer.write(0);
+  radioSer.write(0);
+  radioSer.write(3);
+  uint32_t freq = 0;
+
+  auto r = read_with_wait();
+  freq += (r>>4)*100 + (r&0xf) * 10;
+  freq *= 100;
+
+  r = read_with_wait();
+  freq += (r>>4)*100 + (r&0xf) * 10;
+  freq *= 100;
+
+  r = read_with_wait();
+  freq += (r>>4)*100 + (r&0xf) * 10;
+  freq *= 100;
+
+  r = read_with_wait();
+  freq += (r>>4)*100 + (r&0xf) * 10;
+
+  Serial.print(freq);
+  Serial.print("\t");
+
+  auto mode = read_with_wait();
+  switch (mode) {
+    case 0:
+      Serial.println("LSB"); break;
+    case 1:
+      Serial.println("USB"); break;
+    case 2:
+      Serial.println("CW"); break;
+    case 3:
+      Serial.println("CWR"); break;
+    case 0x82:
+      Serial.println("CW-N"); break;
+    case 4:
+      Serial.println("AM"); break;
+    case 6:
+      Serial.println("WFM"); break;
+    case 8:
+      Serial.println("FM"); break;
+    case 0x88:
+      Serial.println("NFM"); break;
+    case 0xa:
+      Serial.println("DIG"); break;
+    case 0xc:
+      Serial.println("PKT"); break;
+    default:
+      Serial.println("Unknown mode");
+  }
+
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  taskHeartbeat();
+  
+}
